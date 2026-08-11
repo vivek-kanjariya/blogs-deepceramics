83e0abaf63b0a66b117d5b883cd45cda5b25121b
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2026-08-11 07:35:57 +0530

    Use Node.js runtime for Inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,7 +2,7 @@ import { inngest } from "@/lib/inngest/client";
 import workflow from "@/lib/inngest/workflow";
 import { serve } from "inngest/next";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
