9826e2f59042a8653b49c4d8ba88dfa0dc311111
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2026-08-11 09:11:58 +0530

    add force-dynamic to inngest route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
 import workflow from "@/lib/inngest/workflow";
 import { serve } from "inngest/next";
-
 export const runtime = "nodejs";
-
+export const dynamic = "force-dynamic";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [workflow],
--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,4 +2,5 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   id: "workflow-kit-next-demo",
+  aiMetadata: false,
 });
